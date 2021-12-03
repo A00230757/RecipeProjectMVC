07ca10f555c420a8752b439fc8006beda371adf8
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-12-03 15:16:02 -0500

    Integration tests are added in the RecipeProject. Although these are not requires at this stage but i added title tests of all the pages in recipeproject . More integration tests will be added in the next check ins.

---
+++ RecipeProjectBrowserTests/UnitTest1.cs
@@ -0,0 +1,81 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+
+namespace RecipeProjectBrowserTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver _webDriver;
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _webDriver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void TitleTestOfIndexPage()
+        {
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Recipes");
+            Assert.IsTrue(_webDriver.Title.Contains("Index - Recipe Project"));
+            Assert.AreEqual("Index - Recipe Project", _webDriver.Title);
+        }
+
+        [TestMethod]
+        public void TitleTestOfCreateRecipePage()
+        {
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Recipes/Create");
+            Assert.IsTrue(_webDriver.Title.Contains("Create - Recipe Project"));
+            Assert.AreEqual("Create - Recipe Project", _webDriver.Title);
+        }
+
+        [TestMethod]
+        public void TitleTestOfEditRecipePage()
+        {
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Recipes/Edit/1");
+            Assert.IsTrue(_webDriver.Title.Contains("Edit - Recipe Project"));
+            Assert.AreEqual("Edit - Recipe Project", _webDriver.Title);
+        }
+        [TestMethod]
+        public void TitleTestOfDeleteRecipePage()
+        {
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Recipes/Delete/1");
+            Assert.IsTrue(_webDriver.Title.Contains("Delete - Recipe Project"));
+            Assert.AreEqual("Delete - Recipe Project", _webDriver.Title);
+        }
+        [TestMethod]
+        public void TitleTestOfDetailsRecipePage()
+        {
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Recipes/Details/1");
+            Assert.IsTrue(_webDriver.Title.Contains("Details - Recipe Project"));
+            Assert.AreEqual("Details - Recipe Project", _webDriver.Title);
+            Console.WriteLine(_webDriver.Title);
+        }
+        [TestMethod]
+        public void TitleTestOfHomeRecipePage()
+        {
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/");
+            Assert.IsTrue(_webDriver.Title.Contains("Home Page - Recipe Project"));
+            Assert.AreEqual("Home Page - Recipe Project", _webDriver.Title);
+        }
+        [TestCleanup]
+        public void Teardown()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
